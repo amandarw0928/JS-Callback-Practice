7dcb12e324084e6517ab5d35f3b881af2a550fe1
Amanda Wasinger <amandarm0928@outlook.com>
2022-08-24 18:05:19 -0500

    added interval functions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const character = newImage('assets/green-character/static.gif')
 let direction = null;
 let x = 100;
 let y = 250;
+move(character).to(100, 250)
 
 function moveCharacter() {
     if (direction === 'west') {
@@ -22,8 +23,23 @@ function moveCharacter() {
     character.style.left = x + 'px'
     character.style.bottom = y + 'px'
 }
-move(character).to(100, 250)
 
+setInterval(function () {
+    if (direction === 'west') {
+        x = x - 1
+    }
+    if (direction === 'north') {
+        y = y + 1
+    }
+    if (direction === 'east') {
+        x = x + 1
+    }
+    if (direction === 'south') {
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
