020ce49b11bbf56d78528c7ffac24cd96f3fe76a
Amanda Wasinger <amandarm0928@outlook.com>
2022-08-24 19:49:53 -0500

    getting close

--- move.js
+++ move.js
@@ -1,12 +1,10 @@
 function move(element) {
     element.style.position = 'fixed'
-
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
-
-    function moveWithArrowKeys(left, bottom) {
+    function moveWithArrowKeys(left, bottom, callback) {
         let direction = null;
         let x = left;
         let y = bottom;
@@ -30,9 +28,12 @@ function move(element) {
             element.style.left = x + 'px'
             element.style.bottom = y + 'px'
         }
+
         setInterval(moveCharacter, 1)
+
         document.addEventListener('keydown', function (e) {
             if (e.repeat) return;
+
             if (e.key === 'ArrowLeft') {
                 direction = 'west'
             }
@@ -45,14 +46,18 @@ function move(element) {
             if (e.key === 'ArrowDown') {
                 direction = 'south'
             }
+            callback()
         })
+
         document.addEventListener('keyup', function (e) {
             direction = null
+            callback()
         })
     }
 
-    return {
-        to: moveToCoordinates
+}
+
+return {
+    to: moveToCoordinates
         withArrowKeys: moveWithArrowKeys
-    }
 }
